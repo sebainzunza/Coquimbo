aa3f9e0f7948ff50745dd14752fbf1aca96b7352
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2021-10-20 18:40:54 -0300

    Update figuras_ODV1p7.py

--- figuras_ODV1p7.py
+++ figuras_ODV1p7.py
@@ -3,5 +3,7 @@
 # > python3
 #
 import opendrift
-o = opendrift.open('seed1000cada1hr_15dias.nc')
+import numpy as np
+
+o = opendrift.open('seed1000cada1hr_15dias.nc', elements=np.arange(0,392000,100)
 o.plot(filename='figura1.png',fast=True)
